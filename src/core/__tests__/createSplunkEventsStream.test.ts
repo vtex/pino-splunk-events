c78d9bf36d579755c30ced70581cb57eaf16ddb1
gtkatakura <gt.katakura@gmail.com>
2021-05-24 11:11:30 -0300

    chore: using useCases into createSplunkEventsStream tests

--- src/core/__tests__/createSplunkEventsStream.test.ts
+++ src/core/__tests__/createSplunkEventsStream.test.ts
@@ -47,269 +47,149 @@ describe('createSplunkEventsStream', () => {
   }
 
   describe('when the event is some pino log', () => {
-    describe('and has level 10 (trace)', () => {
-      it('calls /services/collector/event with event', async () => {
-        const mockedRoute = mockEventRoute()
+    const useCases = [
+      {
+        level: {
+          value: 10,
+          label: 'trace',
+        },
+        expectedEvent: {
+          level: 'Debug',
+          type: 'Info',
+          workflowInstance: 'workflow-instance-10',
+          workflowType: 'workflow-type-10',
+          account: '',
+          time: '2021-10-15T00:00:00.000Z',
+        },
+      },
 
-        const stream = getStream()
+      {
+        level: {
+          value: 20,
+          label: 'debug',
+        },
+        expectedEvent: {
+          level: 'Debug',
+          type: 'Info',
+          workflowInstance: 'workflow-instance-20',
+          workflowType: 'workflow-type-20',
+          account: '',
+          time: '2021-10-15T00:00:00.000Z',
+        },
+      },
 
-        stream.write(
-          JSON.stringify({
-            level: 10,
-            time: Date.now(),
-            workflowInstance: 'workflow-instance-10',
-            workflowType: 'workflow-type-10',
-          })
-        )
-        stream.write('\n')
+      {
+        level: {
+          value: 30,
+          label: 'info',
+        },
+        expectedEvent: {
+          level: 'Important',
+          type: 'Info',
+          workflowInstance: 'workflow-instance-30',
+          workflowType: 'workflow-type-30',
+          account: '',
+          time: '2021-10-15T00:00:00.000Z',
+        },
+      },
 
-        jest.advanceTimersByTime(100)
+      {
+        level: {
+          value: 40,
+          label: 'warn',
+        },
+        expectedEvent: {
+          level: 'Important',
+          type: 'Warn',
+          workflowInstance: 'workflow-instance-40',
+          workflowType: 'workflow-type-40',
+          account: '',
+          time: '2021-10-15T00:00:00.000Z',
+        },
+      },
 
-        await mockedRoute.waitToHaveBeenCalledTimes(1)
+      {
+        level: {
+          value: 50,
+          label: 'error',
+        },
+        expectedEvent: {
+          level: 'Important',
+          type: 'Error',
+          workflowInstance: 'workflow-instance-50',
+          workflowType: 'workflow-type-50',
+          account: '',
+          time: '2021-10-15T00:00:00.000Z',
+        },
+      },
 
-        expect(mockedRoute).toHaveBeenCalledWith([
-          {
-            event: {
-              account: '',
-              level: 'Debug',
-              time: '2021-10-15T00:00:00.000Z',
-              type: 'Info',
-              workflowInstance: 'workflow-instance-10',
-              workflowType: 'workflow-type-10',
-            },
-            host: '-',
-            sourcetype: 'log',
-          },
-        ])
-      })
-    })
+      {
+        level: {
+          value: 60,
+          label: 'fatal',
+        },
+        expectedEvent: {
+          level: 'Critical',
+          type: 'Error',
+          workflowInstance: 'workflow-instance-60',
+          workflowType: 'workflow-type-60',
+          account: '',
+          time: '2021-10-15T00:00:00.000Z',
+        },
+      },
 
-    describe('and has level 20 (debug)', () => {
-      it('calls /services/collector/event with event', async () => {
-        const mockedRoute = mockEventRoute()
-
-        const stream = getStream()
-
-        stream.write(
-          JSON.stringify({
-            level: 20,
-            time: Date.now(),
-            workflowInstance: 'workflow-instance-20',
-            workflowType: 'workflow-type-20',
-          })
-        )
-        stream.write('\n')
-
-        jest.advanceTimersByTime(100)
-
-        await mockedRoute.waitToHaveBeenCalledTimes(1)
-
-        expect(mockedRoute).toHaveBeenCalledWith([
-          {
-            event: {
-              account: '',
-              level: 'Debug',
-              time: '2021-10-15T00:00:00.000Z',
-              type: 'Info',
-              workflowInstance: 'workflow-instance-20',
-              workflowType: 'workflow-type-20',
-            },
-            host: '-',
-            sourcetype: 'log',
-          },
-        ])
-      })
-    })
-
-    describe('and has level 30 (info)', () => {
-      it('calls /services/collector/event with event', async () => {
-        const mockedRoute = mockEventRoute()
-
-        const stream = getStream()
-
-        stream.write(
-          JSON.stringify({
-            level: 30,
-            time: Date.now(),
-            workflowInstance: 'workflow-instance-30',
-            workflowType: 'workflow-type-30',
-          })
-        )
-        stream.write('\n')
-
-        jest.advanceTimersByTime(100)
-
-        await mockedRoute.waitToHaveBeenCalledTimes(1)
-
-        expect(mockedRoute).toHaveBeenCalledWith([
-          {
-            event: {
-              account: '',
-              level: 'Important',
-              time: '2021-10-15T00:00:00.000Z',
-              type: 'Info',
-              workflowInstance: 'workflow-instance-30',
-              workflowType: 'workflow-type-30',
-            },
-            host: '-',
-            sourcetype: 'log',
-          },
-        ])
-      })
-    })
-
-    describe('and has level 40 (warn)', () => {
-      it('calls /services/collector/event with event', async () => {
-        const mockedRoute = mockEventRoute()
-
-        const stream = getStream()
-
-        stream.write(
-          JSON.stringify({
-            level: 40,
-            time: Date.now(),
-            workflowInstance: 'workflow-instance-40',
-            workflowType: 'workflow-type-40',
-          })
-        )
-        stream.write('\n')
-
-        jest.advanceTimersByTime(100)
-
-        await mockedRoute.waitToHaveBeenCalledTimes(1)
-
-        expect(mockedRoute).toHaveBeenCalledWith([
-          {
-            event: {
-              account: '',
-              level: 'Important',
-              time: '2021-10-15T00:00:00.000Z',
-              type: 'Warn',
-              workflowInstance: 'workflow-instance-40',
-              workflowType: 'workflow-type-40',
-            },
-            host: '-',
-            sourcetype: 'log',
-          },
-        ])
-      })
-    })
-
-    describe('and has level 50 (error)', () => {
-      it('calls /services/collector/event with event', async () => {
-        const mockedRoute = mockEventRoute()
-
-        const stream = getStream()
-
-        stream.write(
-          JSON.stringify({
-            level: 50,
-            time: Date.now(),
-            workflowInstance: 'workflow-instance-50',
-            workflowType: 'workflow-type-50',
-          })
-        )
-        stream.write('\n')
-
-        jest.advanceTimersByTime(100)
-
-        await mockedRoute.waitToHaveBeenCalledTimes(1)
-
-        expect(mockedRoute).toHaveBeenCalledWith([
-          {
-            event: {
-              account: '',
-              level: 'Important',
-              time: '2021-10-15T00:00:00.000Z',
-              type: 'Error',
-              workflowInstance: 'workflow-instance-50',
-              workflowType: 'workflow-type-50',
-            },
-            host: '-',
-            sourcetype: 'log',
-          },
-        ])
-      })
-    })
-
-    describe('and has level 60 (fatal)', () => {
-      it('calls /services/collector/event with event', async () => {
-        const mockedRoute = mockEventRoute()
-
-        const stream = getStream()
-
-        stream.write(
-          JSON.stringify({
-            level: 60,
-            time: Date.now(),
-            workflowInstance: 'workflow-instance-60',
-            workflowType: 'workflow-type-60',
-          })
-        )
-        stream.write('\n')
-
-        jest.advanceTimersByTime(100)
-
-        await mockedRoute.waitToHaveBeenCalledTimes(1)
-
-        expect(mockedRoute).toHaveBeenCalledWith([
-          {
-            event: {
-              account: '',
-              level: 'Critical',
-              time: '2021-10-15T00:00:00.000Z',
-              type: 'Error',
-              workflowInstance: 'workflow-instance-60',
-              workflowType: 'workflow-type-60',
-            },
-            host: '-',
-            sourcetype: 'log',
-          },
-        ])
-      })
-    })
-
-    describe('and has level 100 (unknown)', () => {
-      it('calls /services/collector/event with event', async () => {
-        const mockedRoute = mockEventRoute()
-
-        const stream = getStream()
-
-        stream.write(
-          JSON.stringify({
+      {
+        level: {
+          value: 100,
+          label: 'unknown',
+        },
+        expectedEvent: {
+          level: 'Important',
+          type: 'Warn',
+          workflowInstance: 'invalid-pino-log',
+          workflowType: 'fallback-log',
+          account: '',
+          msg: {
             level: 100,
-            time: Date.now(),
+            time: +new Date('2021-10-15T00:00:00.000Z'),
             workflowInstance: 'workflow-instance-100',
             workflowType: 'workflow-type-100',
-          })
-        )
-        stream.write('\n')
-
-        jest.advanceTimersByTime(100)
-
-        await mockedRoute.waitToHaveBeenCalledTimes(1)
-
-        expect(mockedRoute).toHaveBeenCalledWith([
-          {
-            event: {
-              account: '',
-              level: 'Important',
-              msg: {
-                level: 100,
-                time: +new Date('2021-10-15T00:00:00.000Z'),
-                workflowInstance: 'workflow-instance-100',
-                workflowType: 'workflow-type-100',
-              },
-              type: 'Warn',
-              workflowInstance: 'invalid-pino-log',
-              workflowType: 'fallback-log',
-            },
-            host: '-',
-            sourcetype: 'log',
           },
-        ])
+        },
+      },
+    ]
+
+    for (const { level, expectedEvent } of useCases) {
+      describe(`and has level ${level.value} (${level.label})`, () => {
+        it('calls /services/collector/event with event', async () => {
+          const mockedRoute = mockEventRoute()
+
+          const stream = getStream()
+
+          stream.write(
+            JSON.stringify({
+              level: level.value,
+              time: Date.now(),
+              workflowInstance: `workflow-instance-${level.value}`,
+              workflowType: `workflow-type-${level.value}`,
+            })
+          )
+          stream.write('\n')
+
+          jest.advanceTimersByTime(100)
+
+          await mockedRoute.waitToHaveBeenCalledTimes(1)
+
+          expect(mockedRoute).toHaveBeenCalledWith([
+            {
+              event: expectedEvent,
+              host: '-',
+              sourcetype: 'log',
+            },
+          ])
+        })
       })
-    })
+    }
   })
 
   describe('when has events with valid JSON', () => {
@@ -333,24 +213,24 @@ describe('createSplunkEventsStream', () => {
         expect(mockedRoute).toHaveBeenCalledWith([
           {
             event: {
-              account: '',
               level: 'Important',
-              msg: 'Message encoded as JSON string',
               type: 'Warn',
               workflowInstance: 'invalid-pino-log',
               workflowType: 'fallback-log',
+              account: '',
+              msg: 'Message encoded as JSON string',
             },
             host: '-',
             sourcetype: 'log',
           },
           {
             event: {
-              account: '',
               level: 'Important',
-              msg: ['Another message encoded as JSON string'],
               type: 'Warn',
               workflowInstance: 'invalid-pino-log',
               workflowType: 'fallback-log',
+              account: '',
+              msg: ['Another message encoded as JSON string'],
             },
             host: '-',
             sourcetype: 'log',
